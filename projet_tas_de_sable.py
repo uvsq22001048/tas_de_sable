94620c4b9326b0fdd47d20b01c3ca1fb3e579de7
Ceylia dupin <ceylia.dupin@ens.uvsq.fr>
2022-02-08 10:37:26 +0100

    début de programmation

--- projet_tas_de_sable.py
+++ projet_tas_de_sable.py
@@ -1,7 +1,21 @@
 ###########################
 # groupe BI TD3
-# ceylia DUPIN
+# Ceylia DUPIN
 # Laetitia SALE
 # Louis REBÊCHE
 # Lou GUEVEL
 ###########################
+
+##########
+# Importation de la librairie
+import tkinter as tk
+
+
+racine = tk.Tk()
+racine.title("tas de sable")
+canvas = tk.Canvas(racine, width=600, height=600)
+canvas.grid(column=0, row=0)
+
+
+# boucle principale
+racine.mainloop()
