2744fad45f2ce2ab50984617fe76a6be9ab6fb13
Ceylia dupin <ceylia.dupin@ens.uvsq.fr>
2022-02-08 11:11:59 +0100

    programmation bouton

--- projet_tas_de_sable.py
+++ projet_tas_de_sable.py
@@ -9,13 +9,20 @@
 ##########
 # Importation de la librairie
 import tkinter as tk
-
+from tkinter import * 
+"j'ai du utiliser * car je n'arrivai pas a importer button"
 ###############
 # programme principale
 racine = tk.Tk()
 racine.title("tas de sable")
 canvas = tk.Canvas(racine, width=600, height=600)
 canvas.grid(column=0, row=0)
+
+# creation d'un bouton
+bt_blue = Button(racine, text="configuration")
+bt_blue_w = canvas.create_window(40, 20, window=bt_blue)
+ 
+ 
 
 
 # boucle principale
